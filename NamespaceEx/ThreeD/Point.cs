d3509fac8c4d9aaa8f9f4f66834e1c3d76eb3627
Raul Ulici <raululici@yahoo.com>
2021-11-12 09:56:04 +0200

    Added 2 folders

---
+++ NamespaceEx/ThreeD/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamespaceEx.ThreeD
+{
+    class Point
+    {
+        public int X;
+        public int Y;
+        public int Z;
+    }
+}
